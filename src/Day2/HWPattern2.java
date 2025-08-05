0b0c07598e92aa9338d94996717b03f713e9373e
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2025-08-05 17:43:04 +0530

    day3

--- src/Day2/HWPattern2.java
+++ src/Day2/HWPattern2.java
@@ -3,16 +3,26 @@ package Day2;
 public class HWPattern2 {
 
 	public static void main(String[] args) {
-		for(int i=1;i<=3;i++) {
-			for(int j=0;j<=i+j;j++) {
+		int space=4;
+		for(int i=0;i<4;i++) {
+			space--;
+			for(int k=space; k>0; k--) {
 				System.out.print(" ");
-				for(int k=0;k<=i;k++) {
-					System.out.print("*");
-				}System.out.println();
-			}
+			}for(int j=0;j<=i;j++) {
+				System.out.print("* ");
+				
+			}System.out.println();
+			
 		
 		}
 
 	}
 
 }
+/* output
+   * 
+  * * 
+ * * * 
+* * * * 
+
+*/
